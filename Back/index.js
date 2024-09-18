d3d68075c3e8f9dfdfc4090e7f240319616fb2db
amboez <abmoezashraf@gmail.com>
2024-09-18 16:42:26 +0300

    server setup

---
+++ Back/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// import { connectDB } from "./db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(
+  cors({
+    origin: '*',
+  })
+);
+app.use(bodyParser.text({ type: 'text/xml' }));
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+// connectDB()
+//   .then(() =>
+//     app.listen(PORT, () => {
+//       console.log(`Server started at ${PORT}`)
+//     }))
+//   .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
